a905211eb7a61d99adb1bc847348e9d47decaf48
Kunaldeorukhakar <86115450+Kunaldeorukhakar@users.noreply.github.com>
2024-11-09 08:13:15 -0500

    Update Subdomaintracking.sh
    Added new tool and optimized

--- Subdomaintracking.sh
+++ Subdomaintracking.sh
@@ -14,10 +14,13 @@ mkdir $target
 cd $target
 
 
-subfinder -d $target -t 100 -v | anew  allsub.txt |tee -a newsubdomain_$(date +"%Y%m%d_%H%M%S").txt | notify -pc ~/Documents/BugBounty/$pc | tee -a allsub.txt
+echo "[*] Scanning and finding new subdomains..."
+subfinder -d $target -t 100 -v;python3 ~/Documents/tools/github-subdomains.py -t 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 -d $target | anew  allsub.txt |tee -a newsubdomain_$(date +"%Y%m%d_%H%M%S").txt | notify -pc ~/Documents/BugBounty/$pc | tee -a allsub.txt
 
-
+echo "[*] Scanning for any changes in applications/servers..."
 httpx -status-code -title -tech-detect -cl -list allsub.txt | anew  httpx.txt |tee -a newhttpx_$(date +"%Y%m%d_%H%M%S").txt | notify -pc ~/Documents/BugBounty/$pc | tee -a httpx.txt
 
+echo "[*] Scanning and finding new Github secrets..."
+trufflehog github --org=$target --results=verified | anew gitsecrets |tee -a newgitsecrets_$(date +"%Y%m%d_%H%M%S").txt | notify -pc ~/Documents/BugBounty/$pc | tee -a gitsecrets.txt
 
 echo "Finished subdomain tracking of $target on $(date)" | notify -pc ~/Documents/BugBounty/$pc
