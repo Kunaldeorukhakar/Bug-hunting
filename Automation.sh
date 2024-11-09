03f81805839c72d2a088017dbb0e5c5897b1558b
Kunaldeorukhakar <86115450+Kunaldeorukhakar@users.noreply.github.com>
2024-11-09 08:10:26 -0500

    Update Automation.sh
    added new tool

--- Automation.sh
+++ Automation.sh
@@ -16,10 +16,12 @@ cd $target
 printf "[+] Horizontal domain correlation/acquisitions"
 printf "Searching horizontal domains..."
 email=$(whois $target | grep "Registrant Email" | egrep -ho "[[:graph:]]+@[[:graph:]]+")
-curl -s -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36" "https://viewdns.info/reversewhois/?q=$email" | html2text | grep -Po "[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)" | tail -n +4  | head -n -1 
+curl -s -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36" "https://viewdns.info/reversewhois/?q=$email" | html2text | grep -Po "[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)" | tail -n +4  | head -n -1 | tee -a acquisation.txt
 
 echo "[*] Finding subdomains..."
 subfinder -d $target -t 100 -v -o allsub.txt
+python3 ~/Documents/tools/github-subdomains.py -t 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 -d $target | anew allsub.txt
+
 
 echo "[*] Checking for alive targets using httpx..."
 cat allsub.txt | httpx -t 100 | tee -a probedsub.txt
