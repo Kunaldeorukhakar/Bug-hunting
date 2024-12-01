cc826c0d67f7a173f8a50daf4955729ac9327429
Kunaldeorukhakar <86115450+Kunaldeorukhakar@users.noreply.github.com>
2024-12-01 10:43:46 -0500

    Update Automation3.sh

--- Automation3.sh
+++ Automation3.sh
@@ -20,26 +20,26 @@ echo "[*] Checking for Subdomain Takeover ..."
 subzy run --targets allsub.txt | tee -a Subdomaintakeover.txt
 
 echo "[*] Checking for SQLi using Nuclei fuzzing templates..."
-nuclei -l gfsqli.txt -v -t ~/nuclei-templates/fuzzing-templates/sqli -o nucleifuzzsqli.txt | notify -pc ~/Documents/BugBounty/$pc
+nuclei -l gfsqli.txt -no-httpx -v -t ~/nuclei-templates/fuzzing-templates/sqli -o nucleifuzzsqli.txt | notify -pc ~/Documents/BugBounty/$pc
 
 echo "[*] Checking for LFI using Nuclei fuzzing templates..."
-nuclei -l gflfi.txt -v -t ~/nuclei-templates/fuzzing-templates/lfi -o nucleifuzzlfi.txt | notify -pc ~/Documents/BugBounty/$pc
+nuclei -l gflfi.txt -no-httpx -v -t ~/nuclei-templates/fuzzing-templates/lfi -o nucleifuzzlfi.txt | notify -pc ~/Documents/BugBounty/$pc
 
 echo "[*] Checking for Open-redirect using Nuclei fuzzing templates..."
-nuclei -l gfredirect.txt -v -t ~/nuclei-templates/fuzzing-templates/redirect -o nucleifuzzredirect.txt | notify -pc ~/Documents/BugBounty/$pc
+nuclei -l gfredirect.txt -no-httpx -v -t ~/nuclei-templates/fuzzing-templates/redirect -o nucleifuzzredirect.txt | notify -pc ~/Documents/BugBounty/$pc
 
 echo "[*] Checking for vulnerabilites using Nuclei templates..."
-nuclei -l allsub.txt -v -t ~/nuclei-templates/http/cves -severity critical,high,medium -o nucleicves.txt | notify -pc ~/Documents/BugBounty/$pc
+nuclei -l allsub.txt -no-httpx -v -t ~/nuclei-templates/http/cves -severity critical,high,medium -o nucleicves.txt | notify -pc ~/Documents/BugBounty/$pc
 
-nuclei -l allsub.txt -v -t ~/nuclei-templates/http/cnvd -severity critical,high,medium -o nucleicnvd.txt | notify -pc ~/Documents/BugBounty/$pc
+nuclei -l allsub.txt -no-httpx -v -t ~/nuclei-templates/http/cnvd -severity critical,high,medium -o nucleicnvd.txt | notify -pc ~/Documents/BugBounty/$pc
 
-nuclei -l allsub.txt -v -t ~/nuclei-templates/http/exposed-panels -severity critical,high,medium -o nucleiexposedpanels.txt | notify -pc ~/Documents/BugBounty/$pc
+nuclei -l allsub.txt -no-httpx -v -t ~/nuclei-templates/http/exposed-panels -severity critical,high,medium -o nucleiexposedpanels.txt | notify -pc ~/Documents/BugBounty/$pc
 
-nuclei -l allsub.txt -v -t ~/nuclei-templates/http/takeovers -severity critical,high,medium -o nucleitakeovers.txt | notify -pc ~/Documents/BugBounty/$pc  
+nuclei -l allsub.txt -no-httpx -v -t ~/nuclei-templates/http/takeovers -severity critical,high,medium -o nucleitakeovers.txt | notify -pc ~/Documents/BugBounty/$pc  
 
-nuclei -l allsub.txt -v -t ~/nuclei-templates/http/exposures -severity critical,high,medium -o nucleiexposures.txt | notify -pc ~/Documents/BugBounty/$pc
+nuclei -l allsub.txt -no-httpx -v -t ~/nuclei-templates/http/exposures -severity critical,high,medium -o nucleiexposures.txt | notify -pc ~/Documents/BugBounty/$pc
 
-nuclei -l allsub.txt -v -t ~/nuclei-templates/network -severity critical,high,medium -o nucleinetwork.txt | notify -pc ~/Documents/BugBounty/$pc
+nuclei -l allsub.txt -no-httpx -v -t ~/nuclei-templates/network -severity critical,high,medium -o nucleinetwork.txt | notify -pc ~/Documents/BugBounty/$pc
 
 
 echo "Finished Automation3 for $target on $(date)" | notify -pc ~/Documents/BugBounty/$pc
