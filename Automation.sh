97bd1ab55e1860c7b561f4d8544ad259151a406c
Kunal Deorukhakar <86115450+Kunaldeorukhakar@users.noreply.github.com>
2026-06-28 11:33:02 +0530

    Refactor Automation.sh for improved script execution
    Updated Automation.sh to enhance functionality and improve script performance by modifying command usage and adding timeouts.

--- Automation.sh
+++ Automation.sh
@@ -22,16 +22,25 @@ curl -s -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (
 
 echo "[*] Finding subdomains..."
 subfinder -d $target -t 100 -v -o allsub.txt
-github-subdomains -t $githubtoken -d $target -raw | anew allsub.txt
+github-subdomains -d $target -raw | anew allsub.txt
 
+echo "[*] Permutation submains ..."
+shuffledns -d $target -w /usr/share/wordlists/SecLists/Discovery/DNS/subdomains-top1million-5000.txt -r ~/Downloads/resolvers-trusted.txt -mode bruteforce | anew allsub.txt
+cat allsub.txt | alterx | shuffledns -d $target -r /usr/share/wordlists/SecLists/Miscellaneous/dns-resolvers.txt -mode resolve | anew subs.txt
 
 echo "[*] Checking for alive targets using httpx..."
-cat allsub.txt | httpx -t 100 | tee -a probedsub.txt
-httpx -status-code -title -tech-detect -cl -list allsub.txt -t 100 -o httpx.txt
+cat allsub.txt | httpx -t 100 -duc | tee -a probedsub.txt
 
 echo "[*] Fetching all the URLs using katana and performing pattern matching..."
-katana -u allsub.txt -d 3 -kf -jc -ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg -threads 100 -o allurls.txt
-cat allsub.txt| gau --verbose --threads 10 | grep -vE '\.(woff|css|png|svg|jpg|woff2|jpeg|gif|svg)$' | anew allurls.txt
+timeout 30m katana -list probedsub.txt -d 3 -kf -jc \
+-ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg \
+-duc -o allurls.txt || echo "Katana timed out after 30 minutes"
+
+timeout 30m bash -c \
+"cat probedsub.txt | gau --threads 10 \
+| grep -vE '\.(woff|css|png|svg|jpg|woff2|jpeg|gif|svg)$' \
+| anew allurls.txt" || echo "Gau timed out after 30 minutes"
+
 cat allurls.txt | grep -E "\.js$" | grep -vE "jquery|momentjs|bootstrap|lodash|polyfills|angular|modernizr|react|vue|backbone|ember|knockout|meteor|node|express|socket.io|d3|highcharts|leaflet|phaser" | anew | tee -a js.txt
 cat allurls.txt | gf xss | tee -a gfxss.txt
 cat allurls.txt | gf sqli | tee -a gfsqli.txt
@@ -39,11 +48,13 @@ cat allurls.txt | gf rce | tee -a gfrce.txt
 cat allurls.txt | gf lfi | tee -a gflfi.txt
 cat allurls.txt | gf ssrf | tee -a gfssrf.txt
 cat allurls.txt | gf idor | tee -a gfidor.txt
-cat allurls.txt | gf redirect | tee -a gfredirect.txt
+cat allurls.txt | gf ssti | tee -a gfssti.txt
 
 echo "[*] Fetching all the ip's from hosts and shodan..."
 while read -r domain; do dig +short "$domain"  | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}'; done < allsub.txt | anew ips.txt
 python3 ~/Documents/tools/freedan/freedan.py -S $target | anew ips.txt
+
+httpx -status-code -title -tech-detect -cl -list allsub.txt -t 100 -duc -o httpx.txt
 
 
 echo "Finished Automation1 for $target on $(date)" | notify -pc ~/Documents/BugBounty/$pc
