04fedeeee4677c6b927f1fae59853c95546370f1
Kunal Deorukhakar <86115450+Kunaldeorukhakar@users.noreply.github.com>
2026-06-28 11:33:49 +0530

    Enhance automation script with new checks and scans
    Added checks for subdomain takeover and exposed .git files. Updated port scanning and secret finding commands.

--- Automation2.sh
+++ Automation2.sh
@@ -15,18 +15,23 @@ cd $program
 mkdir $target
 cd $target
 
+echo "[*] Checking for Subdomain Takeover ..."
+subzy run --targets allsub.txt | tee -a Subdomaintakeover.txt
 
-echo "[*] Finding secrets from js files..."
-cat js.txt | while read url; do python3 ~/Documents/tools/SecretFinder/SecretFinder.py -i $url -o cli >> secrets.txt; done
+echo "[*] Finding reflected params for manual testing.."
+cat gfxss.txt | qsreplace KRD | httpx -silent -mr KRD | tee -a reflected.txt
+
+echo "[*] Finding exposed .git files.."
+cat allsub.txt | httpx -sc -server -cl -path "/.git/" -mc 200 -location -ms "Index of" -probe | tee -a dotgitexposed.txt
+
+#echo "[*] Finding secrets from js files..."
+#cat js.txt | while read url; do python3 ~/Documents/tools/SecretFinder/SecretFinder.py -i $url -o cli >> secrets.txt; done
 
 echo "[*] Finding secrets from Github ..."
-trufflehog github --org=$org --results=verified --no-update | tee -a gitsecrets.txt
+trufflehog github --org=$org --token=$GITHUB_TOKEN --results=verified --no-update | tee -a gitsecrets.txt
 
-echo "[*] Fuzzing for sensitive files/directories..."
-ffuf -u https://$target/FUZZ -w /usr/share/wordlists/WordList/god.txt -e sql,config,xml,json
-
-echo "[*] Finding open ports using rustscan..."
-rustscan -a ips.txt --range 1-1000 --ulimit 3000 -- -Pn | tee -a nmap.txt
+echo "[*] Finding open ports ..."
+naabu -list ips.txt -top-ports 1000 -Pn -rate 1000 -o open_ports.txt 
 
 echo "[*] Enumerating cloud environment..."
 enumkey="${target%%.*}"
