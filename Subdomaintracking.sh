9bbbcfed2092a97ddd3456f9aadea72d188df807
Kunal Deorukhakar <86115450+Kunaldeorukhakar@users.noreply.github.com>
2026-06-28 11:34:58 +0530

    Refactor Subdomaintracking.sh for better output handling
    Updated subdomain tracking script to improve output logging and command usage.

--- Subdomaintracking.sh
+++ Subdomaintracking.sh
@@ -15,18 +15,44 @@ cd $program
 mkdir $target
 cd $target
 
+# Output file
+output_file="new_sub.txt"
+
+echo "Scan Date: $(date +"%d-%m-%Y %H:%M:%S")" >> "$output_file"
+echo "-------------------------------------" >> "$output_file"
 
 echo "[*] Scanning and finding new subdomains..."
-subfinder -d $target -t 100 -v;github-subdomains -t $github_token -d $target | anew  allsub.txt |tee -a newsubdomain_$(date +"%d;%m;%Y_%H;%M;%S").txt | notify -pc ~/Documents/BugBounty/$pc | tee -a allsub.txt
+# old
+# subfinder -d $target -t 100 -v;github-subdomains -t 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 -d $target | anew  allsub.txt |tee -a newsubdomain_$(date +"%d;%m;%Y_%H;%M;%S").txt | notify -pc ~/Documents/BugBounty/$pc
+
+# new
+echo "--------------Subdomains-------------" >> $output_file
+subfinder -d $target -t 100 -silent | shuffledns -d $target -r /usr/share/wordlists/SecLists/Miscellaneous/dns-resolvers.txt -mode resolve | anew allsub.txt | tee -a $output_file
+
 
 echo "[*] Scanning for any changes in applications/servers..."
-httpx -status-code -title -tech-detect -cl -list allsub.txt | anew httpx.txt |tee -a newhttpx_$(date +"%d;%m;%Y_%H;%M;%S").txt | notify -pc ~/Documents/BugBounty/$pc | tee -a httpx.txt
+# old
+# httpx -status-code -title -tech-detect -cl -list allsub.txt -duc | anew httpx.txt |tee -a newhttpx_$(date +"%d;%m;%Y_%H;%M;%S").txt | notify -pc ~/Documents/BugBounty/$pc
+
+#new
+echo "--------------URLs-------------" >> $output_file
+httpx -status-code -title -tech-detect -cl -list allsub.txt -duc | anew httpx.txt | tee -a $output_file
 
 echo "[*] Scanning and finding new Github secrets..."
-trufflehog github --org=$org --results=verified --no-update | anew gitsecrets.txt |tee -a newgitsecrets_$(date +"%d;%m;%Y_%H;%M;%S").txt | notify -pc ~/Documents/BugBounty/$pc | tee -a gitsecrets.txt
+# old
+# trufflehog github --org=$org --results=verified --no-update | anew gitsecrets.txt |tee -a newgitsecrets_$(date +"%d;%m;%Y_%H;%M;%S").txt | notify -pc ~/Documents/BugBounty/$pc
+
+# new
+echo "--------------Secrets-------------" >> $output_file
+trufflehog github --org=$org --token=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 --results=verified --no-update | anew gitsecrets.txt | tee -a $output_file
+
 
 echo "[*] Checking for Subdomain takeover..."
-subzy run --targets allsub.txt | tee -a newSubdomaintakeover_$(date +"%d;%m;%Y_%H;%M;%S").txt
+# old
+# subzy run --targets allsub.txt --hide_fails | tee -a newSubdomaintakeover_$(date +"%d;%m;%Y_%H;%M;%S").txt
 
+# new
+echo "--------------Subdomain-Takeover-------------" >> $output_file
+subzy run --targets allsub.txt --hide_fails | anew Subdomaintakeover.txt | tee -a $output_file
 
 echo "Finished subdomain tracking of $target on $(date)" | notify -pc ~/Documents/BugBounty/$pc
